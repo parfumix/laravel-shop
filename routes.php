c7f5034aaad0753e0fe4dd0b7f73d23298b09524
Radu <radu.bordei89@gmail.com>
2015-10-19 10:45:23 +0300

    fix.

--- routes.php
+++ routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['prefix' => 'admin/shop', 'as' => 'shop::', 'middleware' => 'role:admin', 'namespace' => 'Flysap\Shop\Controllers\Admin'], function() {
+Route::group(['prefix' => 'admin/shop', 'as' => 'shop::', 'middleware' => 'role:admin', 'namespace' => 'Laravel\Shop\Controllers\Admin'], function() {
 
     Route::get('products', ['as' => 'shop-products', 'uses' => 'ProductController@lists']);
 });
