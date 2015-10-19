575a9d45c51b0aa2b1087a097215c4e0834dd9c4
Radu <radu.bordei89@gmail.com>
2015-10-19 10:24:29 +0300

    adding namespace.

--- routes.php
+++ routes.php
@@ -1,5 +1,6 @@
 <?php
 
-Route::group(['prefix' => 'admin/shop', 'as' => 'shop::', 'middleware' => 'role:admin'], function() {
+Route::group(['prefix' => 'admin/shop', 'as' => 'shop::', 'middleware' => 'role:admin', 'namespace' => 'Flysap\Media\Controllers\Admin'], function() {
 
+    Route::get('products', ['as' => 'shop-products', 'uses' => 'ProductController@lists']);
 });
