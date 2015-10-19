d968cf311450a6b71eb48bbf64cdcf637622c55a
Radu <radu.bordei89@gmail.com>
2015-10-19 11:25:17 +0300

    implement sortable.

--- src/Product.php
+++ src/Product.php
@@ -6,15 +6,17 @@ use Eloquent\ImageAble\ImageAble;
 use Eloquent\ImageAble\ImageAbleTrait;
 use Eloquent\Meta\MetaAble;
 use Eloquent\Meta\MetaTrait;
+use Eloquent\Sortable\Sortable;
+use Eloquent\Sortable\SortableTrait;
 use Eloquent\Translatable\Translatable;
 use Eloquent\Translatable\TranslatableTrait;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Meta\Eloquent\MetaSeoable;
 use Laravel\Meta\Eloquent\MetaSeoTrait;
 
-class Product extends Model implements ImageAble, Translatable, MetaAble, MetaSeoable {
+class Product extends Model implements ImageAble, Translatable, MetaAble, MetaSeoable, Sortable {
 
-    use ImageAbleTrait, TranslatableTrait, MetaTrait, MetaSeoTrait;
+    use ImageAbleTrait, TranslatableTrait, MetaTrait, MetaSeoTrait, SortableTrait;
 
     /**
      * @var array
