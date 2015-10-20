50ea621c90559eff6c3101a2d614cd52e535bd33
Radu <radu.bordei89@gmail.com>
2015-10-20 11:21:44 +0300

    improve routes.

--- routes.php
+++ routes.php
@@ -3,4 +3,7 @@
 Route::group(['prefix' => 'admin/shop', 'as' => 'shop::', 'middleware' => 'role:admin', 'namespace' => 'Laravel\Shop\Controllers\Admin'], function() {
 
     Route::get('products', ['as' => 'shop-products', 'uses' => 'ProductController@lists']);
+    Route::match(['get', 'post'],'products/create', ['as' => 'create-product', 'uses' => 'ProductController@create']);
+    Route::get('products/edit/{id}', ['as' => 'edit-product', 'uses' => 'ProductController@edit']);
+    Route::get('products/delete/{id}', ['as' => 'delete-product', 'uses' => 'ProductController@delete']);
 });
