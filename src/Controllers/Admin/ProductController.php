3e3b181faa1dd9fb8f97310aa6c4a77f8353630e
Radu <radu.bordei89@gmail.com>
2015-10-20 11:22:27 +0300

    improve products controller.

--- src/Controllers/Admin/ProductController.php
+++ src/Controllers/Admin/ProductController.php
@@ -4,6 +4,7 @@ namespace Laravel\Shop\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Eloquent\Sortable\Sortable;
+use Illuminate\Http\Request;
 use Laravel\Shop\Product;
 use Parfumix\TableManager;
 use Parfumix\FormBuilder;
@@ -31,6 +32,63 @@ class ProductController extends Controller {
             ]
         );
 
-        return view('shop::products', compact('table'));
+        return view('shop::products.lists', compact('table'));
+    }
+
+    /**
+     * Create new product .
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector|\Illuminate\View\View
+     */
+    public function create(Request $request) {
+        if( $request->isMethod(Request::METHOD_POST) ) {
+            $row = $this->repository->create(
+                $request->all()
+            );
+
+            $row->save();
+
+            return redirect(
+                route('shop::edit-product', ['id' => $row->id])
+            );
+        }
+
+        $form = FormBuilder\create_form(['method' => 'post', 'action' => '']);
+        $form->addElements(
+            $this->repository->skyEdit()
+        );
+
+        return view('shop::products.create', compact('form'));
+    }
+
+    /**
+     * Edit product .
+     *
+     * @param $id
+     * @return \Illuminate\View\View
+     */
+    public function edit($id) {
+        $row = $this->repository->find($id);
+
+        $form = FormBuilder\create_form(['method' => 'post', 'action' => '']);
+        $form->addElements(
+            $row->skyEdit()
+        );
+
+        return view('shop::products.create', compact('form'));
+    }
+
+    /**
+     * Delete product .
+     *
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function delete($id) {
+        if( $product = $this->repository->find($id) ) {
+            $product->delete();
+        }
+
+        return back();
     }
 }
