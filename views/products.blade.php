1c8fdd48dee442eac4f7737437c3fa5cdcf79787
Radu <radu.bordei89@gmail.com>
2015-10-20 10:46:28 +0300

    improve products view.

--- views/products.blade.php
+++ views/products.blade.php
@@ -3,9 +3,32 @@
 @section('content')
     <!-- Main content -->
     <section class="content">
-        {!! Widget::render() !!}
+
         <!-- Your Page Content Here -->
 
-        {!! $table->render() !!}
+        <div class="row">
+
+            {!! Widget::render() !!}
+
+            <div class="col-xs-12">
+
+                <div class="box">
+                    {!! Parfumix\TableManager\render_filter_form($table) !!}
+                </div>
+
+                <div class="box">
+                    <div class="box-body table-responsive no-padding">
+                        {!! $table->render() !!}
+                        {!! Parfumix\TableManager\render_pagination($table, null, ['scope' => request('scope')]) !!}
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+            </div>
+            <!-- /.col -->
+
+        </div>
+        <!-- /.row -->
+
     </section><!-- /.content -->
 @endsection
