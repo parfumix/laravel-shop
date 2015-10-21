acdc9f37c1720078f1824608be2aecc03d4d2092
Radu <radu.bordei89@gmail.com>
2015-10-21 15:22:09 +0300

    rename migration.

--- migrations/2015_10_15_155360_create_carts_table.php
+++ migrations/2015_10_15_155360_create_carts_table.php
@@ -21,12 +21,10 @@ class CreateCartsTable extends Migration {
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')
-                ->onDelete('cascade')
-                ->onUpdate('cascade');
+                ->onDelete('cascade')->onUpdate('cascade');
 
             $table->foreign('product_id')->references('id')->on('products')
-                ->onDelete('cascade')
-                ->onUpdate('cascade');
+                ->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
