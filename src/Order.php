6bcbbf7085c223368184319953e67336da99feb2
Radu <radu.bordei89@gmail.com>
2015-10-21 18:10:37 +0300

    adding orders.

---
+++ src/Order.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Laravel\Shop;
+
+use Flysap\Scaffold\ScaffoldAble;
+use Flysap\Scaffold\Traits\ScaffoldTrait;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Relations\RelationTrait;
+use Parfumix\FormBuilder as Form;
+
+class Order extends Model implements ScaffoldAble {
+
+    use ScaffoldTrait, RelationTrait;
+
+    /**
+     * @var string
+     */
+    protected $table = 'orders';
+
+    /**
+     * @var bool
+     */
+    public $timestamps = false;
+
+}
