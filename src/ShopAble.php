1044484207136cf9b708fa30d6087e9606cc0920
Radu <radu.bordei89@gmail.com>
2015-10-21 15:02:42 +0300

    adding add / drop product.

--- src/ShopAble.php
+++ src/ShopAble.php
@@ -4,4 +4,21 @@ namespace Laravel\Shop;
 
 interface ShopAble {
 
+    /**
+     * Add product to cart
+     *
+     * @param $product_id
+     * @param array $attributes
+     * @return mixed
+     */
+    public function addProduct($product_id, array $attributes = array());
+
+    /**
+     * Drop product from cart .
+     *
+     * @param $product_id
+     * @param array $attributes
+     * @return mixed
+     */
+    public function dropProduct($product_id, array $attributes = array());
 }
--- src/Traits/ShopTrait.php
+++ src/Traits/ShopTrait.php
@@ -14,4 +14,26 @@ trait ShopTrait {
     public function products() {
         return $this->hasMany(Cart::class, 'id', 'user_id');
     }
+
+    /**
+     * Add product to cart
+     *
+     * @param $product_id
+     * @param array $attributes
+     * @return mixed
+     */
+    public function addProduct($product_id, array $attributes = array()) {
+
+    }
+
+    /**
+     * Drop product from cart .
+     *
+     * @param $product_id
+     * @param array $attributes
+     * @return mixed
+     */
+    public function dropProduct($product_id, array $attributes = array()) {
+
+    }
 }
