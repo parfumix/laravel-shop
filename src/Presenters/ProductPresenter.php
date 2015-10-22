de92c833b4ea3fb5dafb347f1d8dd4d0e2a084d2
Radu <radu.bordei89@gmail.com>
2015-10-22 19:37:28 +0300

    adding format title function.

--- src/Presenters/ProductPresenter.php
+++ src/Presenters/ProductPresenter.php
@@ -6,4 +6,14 @@ use Robbo\Presenter\Presenter;
 
 class ProductPresenter extends Presenter {
 
+    /**
+     * Format title .
+     *
+     * @param null $locale
+     * @return mixed
+     */
+    public function formatTitle($locale = null) {
+        if( $translation = $this->getObject()->translate($locale) )
+            return $translation->title;
+    }
 }
