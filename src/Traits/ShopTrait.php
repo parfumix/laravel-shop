b7ffb674be9dc195b84af4fd893a64cbbb47e523
Radu <radu.bordei89@gmail.com>
2015-10-22 17:58:01 +0300

    some improvement.

--- src/Traits/ShopTrait.php
+++ src/Traits/ShopTrait.php
@@ -3,19 +3,26 @@
 namespace Laravel\Shop\Traits;
 
 use Laravel\Shop\Cart;
+use Laravel\Shop\Events\AddedToCart;
+use Laravel\Shop\Events\RemoveFromCart;
+use Laravel\Shop\Product;
 
 trait ShopTrait {
 
-    /**
-     * On boot add new relation .
-     */
-    public function boot() {
-        if( isset($this->relation) )
-            $relation = $this->relation;
+    #@todo
+    public static function boot()
+    {
+        parent::boot();
 
-        $this->relation = array_merge($relation, [
-            'carts'
-        ]);
+        /*if( isset($this->relation) )
+                $relation = $this->relation;
+
+            $this->relation = array_merge($relation, [
+                'carts'
+            ]);
+            $post->created_by = Auth::user()->id;
+            $post->updated_by = Auth::user()->id;
+        */
     }
 
     /**
@@ -27,30 +34,41 @@ trait ShopTrait {
         return $this->hasMany(Cart::class, 'user_id', 'id');
     }
 
-
     /**
      * Add product to cart
      *
-     * @param $product_id
+     * @param Product $product
      * @param array $attributes
      * @return mixed
      */
-    public function addItem($product_id, array $attributes = array()) {
-        return $this->carts()->save((new Cart([
-            'product_id' => $product_id
-        ] + $attributes)));
+    public function inCart(Product $product, array $attributes = array()) {
+        $cart = (new Cart([
+                'product_id' => $product->id
+            ] + $attributes));
+
+        $this->carts()
+            ->save($cart);
+
+        event(new AddedToCart($cart));
+
+        return $cart;
     }
 
     /**
      * Drop product from cart .
      *
-     * @param $product_id
+     * @param Product $product
      * @return mixed
      */
-    public function dropItem($product_id) {
-        $product = $this->carts()->where('product_id', $product_id)
-            ->first();
+    public function outCart(Product $product) {
+        $carts = $this->carts()
+            ->where('product_id', $product->id)
+            ->get();
 
-        return $product->delete();
+        $carts->each(function($cart) {
+            $cart->delete();
+        });
+
+        event(new RemoveFromCart);
     }
 }
